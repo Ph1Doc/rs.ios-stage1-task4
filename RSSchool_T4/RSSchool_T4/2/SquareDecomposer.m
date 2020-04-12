4da549a90db667f92de3a6511388da74a209bc5e
Oleg Sapun <oleg.sapun@gmail.com>
2020-04-13 00:29:33 +0300

    feat: add new function SquareDecomposer

--- RSSchool_T4/RSSchool_T4/2/SquareDecomposer.m
+++ RSSchool_T4/RSSchool_T4/2/SquareDecomposer.m
@@ -2,7 +2,35 @@
 
 @implementation SquareDecomposer
 - (NSArray <NSNumber*>*)decomposeNumber:(NSNumber*)number {
+
+    if ([number doubleValue] < 5) {
+        return nil;
+    } else {
+        NSMutableArray *result = [[NSMutableArray alloc] initWithArray:[SquareDecomposer findSquaresFor:number with:[number doubleValue] * [number doubleValue]]];
+        [result removeLastObject];
+        return result;
+    }
     // good luck
-    return @[@0];
+}
+
++(NSArray <NSNumber*>*)findSquaresFor:(NSNumber*)number with:(NSInteger)square{
+
+    long i = [number longLongValue];
+
+    if (square == 0) {
+        return [[NSArray alloc] initWithObjects:number, nil];
+    }
+
+    for (long j = i - 1; j > 0; j-- ){
+        if (square - j*j >= 0) {
+            NSMutableArray *squares = [[NSMutableArray alloc] initWithArray:[SquareDecomposer findSquaresFor:[NSNumber numberWithDouble:j] with:(square - j*j)]];
+            if ([squares count] != 0) {
+                [squares addObject:number];
+                return squares;
+            }
+        }
+
+    }
+    return @[];
 }
 @end
